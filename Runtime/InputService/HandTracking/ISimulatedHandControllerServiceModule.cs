e6a3ff47beb2db8a83405a2589e101913625eaa4
Dino Fejzagić <dino.f@live.de>
2022-12-06 19:37:19 +0100

    Add missing scripts

---
+++ Runtime/InputService/HandTracking/ISimulatedHandControllerServiceModule.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Reality Collective. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using RealityToolkit.InputSystem.Interfaces.Modules;
+
+namespace RealityToolkit.DeviceSimulation.InputService.HandTracking
+{
+    public interface ISimulatedHandControllerServiceModule : ISimulatedControllerServiceModule, IMixedRealityHandControllerServiceModule
+    {
+        /// <summary>
+        /// Gets the simulated hand controller pose animation speed controlling
+        /// how fast the hand will translate from one pose to another.
+        /// </summary>
+        float HandPoseAnimationSpeed { get; }
+    }
+}
