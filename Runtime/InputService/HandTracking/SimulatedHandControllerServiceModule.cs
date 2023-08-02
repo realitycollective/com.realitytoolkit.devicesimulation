e56df520600e582071a41a7865f5e2a2153d77fa
Dino Fejzagić <dino.f@live.de>
2023-08-02 16:02:39 +0200

    Fix profile path

--- Runtime/InputService/HandTracking/SimulatedHandControllerServiceModule.cs
+++ Runtime/InputService/HandTracking/SimulatedHandControllerServiceModule.cs
@@ -34,35 +34,35 @@ namespace RealityToolkit.DeviceSimulation.InputService.HandTracking
 
             HandPoseAnimationSpeed = profile.HandPoseAnimationSpeed;
 
-            RenderingMode = profile.RenderingMode != inputServiceProfile.RenderingMode
+            RenderingMode = profile.RenderingMode != inputServiceProfile.HandControllerSettings.RenderingMode
                 ? profile.RenderingMode
-                : inputServiceProfile.RenderingMode;
+                : inputServiceProfile.HandControllerSettings.RenderingMode;
 
-            HandPhysicsEnabled = profile.HandPhysicsEnabled != inputServiceProfile.HandPhysicsEnabled
+            HandPhysicsEnabled = profile.HandPhysicsEnabled != inputServiceProfile.HandControllerSettings.HandPhysicsEnabled
                 ? profile.HandPhysicsEnabled
-                : inputServiceProfile.HandPhysicsEnabled;
+                : inputServiceProfile.HandControllerSettings.HandPhysicsEnabled;
 
-            UseTriggers = profile.UseTriggers != inputServiceProfile.UseTriggers
+            UseTriggers = profile.UseTriggers != inputServiceProfile.HandControllerSettings.UseTriggers
                 ? profile.UseTriggers
-                : inputServiceProfile.UseTriggers;
+                : inputServiceProfile.HandControllerSettings.UseTriggers;
 
-            BoundsMode = profile.BoundsMode != inputServiceProfile.BoundsMode
+            BoundsMode = profile.BoundsMode != inputServiceProfile.HandControllerSettings.BoundsMode
                 ? profile.BoundsMode
-                : inputServiceProfile.BoundsMode;
+                : inputServiceProfile.HandControllerSettings.BoundsMode;
 
-            var isGrippingThreshold = profile.GripThreshold != inputServiceProfile.GripThreshold
+            var isGrippingThreshold = profile.GripThreshold != inputServiceProfile.HandControllerSettings.GripThreshold
                 ? profile.GripThreshold
-                : inputServiceProfile.GripThreshold;
+                : inputServiceProfile.HandControllerSettings.GripThreshold;
 
             if (profile.TrackedPoses != null && profile.TrackedPoses.Count > 0)
             {
-                TrackedPoses = profile.TrackedPoses.Count != inputServiceProfile.TrackedPoses.Count
+                TrackedPoses = profile.TrackedPoses.Count != inputServiceProfile.HandControllerSettings.TrackedPoses.Count
                     ? profile.TrackedPoses
-                    : inputServiceProfile.TrackedPoses;
+                    : inputServiceProfile.HandControllerSettings.TrackedPoses;
             }
             else
             {
-                TrackedPoses = inputServiceProfile.TrackedPoses;
+                TrackedPoses = inputServiceProfile.HandControllerSettings.TrackedPoses;
             }
 
             if (TrackedPoses == null || TrackedPoses.Count == 0)
